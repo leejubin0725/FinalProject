787141245a77d273e540aa76a49b1f1d71b9681a
juseungpark97 <vmffpdlsdnjxj@gmail.com>
2024-07-19 01:24:23 +0900

    SpringBoot Amazon Corretto 17

---
+++ lastProject/src/test/java/com/kh/last/LastProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.last;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LastProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
