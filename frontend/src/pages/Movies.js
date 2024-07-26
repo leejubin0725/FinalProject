a22e793896ecc650041e06332ca73c4248a66bb2
juseungpark97 <vmffpdlsdnjxj@gmail.com>
2024-07-26 09:09:01 +0900

    다운로드방지

--- frontend/src/pages/Movies.js
+++ frontend/src/pages/Movies.js
@@ -20,7 +20,8 @@ const Movies = () => {
           <div key={video.id} className="movie-item">
             <h2>{video.title}</h2>
             <p>{video.description}</p>
-            <video width="320" height="240" controls>
+             //다운로드 방지
+            <video width="320" height="240" controls controlsList="nodownload" onContextMenu={(e) => e.preventDefault()}>
               <source src={video.url} type="video/mp4" />
               Your browser does not support the video tag.
             </video>
--- lastProject/src/main/java/com/kh/last/LastProjectApplication.java
+++ lastProject/src/main/java/com/kh/last/LastProjectApplication.java
@@ -8,18 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LastProjectApplication {
 
     public static void main(String[] args) {
-        // Load environment variables from .env file
-        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-
-        // Set environment variables for the application
-        System.setProperty("DB_URL", dotenv.get("DB_URL"));
-        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
-        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
-        System.setProperty("AWS_ACCESS_KEY_ID", dotenv.get("AWS_ACCESS_KEY_ID"));
-        System.setProperty("AWS_SECRET_ACCESS_KEY", dotenv.get("AWS_SECRET_ACCESS_KEY"));
-        System.setProperty("AWS_S3_BUCKET_NAME", dotenv.get("AWS_S3_BUCKET_NAME"));
-        System.setProperty("AWS_REGION", dotenv.get("AWS_REGION"));
-
         SpringApplication.run(LastProjectApplication.class, args);
     }
 }
