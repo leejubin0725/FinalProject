bcb85f2b0c7c69004d9a53b4e2299616fa0cbd2e
juseungpark97 <vmffpdlsdnjxj@gmail.com>
2024-07-31 17:10:14 +0900

    메인페이지변경중

--- frontend/src/components/HomeRank.tsx
+++ frontend/src/components/HomeRank.tsx
@@ -1,27 +1,25 @@
-import { FunctionComponent } from "react";
-import styles from "./HomeRank.module.css";
+// src/components/HomeRank.tsx
+import { FunctionComponent } from 'react';
+import styles from './HomeRank.module.css';
 
-export type Rank = {
-  className: string;
+export type RankProps = {
+  className?: string;  // className을 선택적으로 설정
 };
 
-const rank: FunctionComponent<Rank> = ({
-  className = "",
-}) => {
+const Rank: FunctionComponent<RankProps> = ({ className = "" }) => {
   return (
     <div className={[styles.sectionTitleParent, className].join(" ")}>
       <div className={styles.sectionTitle}>
-        <h2 className={styles.seeAgain}>랭킹 TOP10  <img
-          className={styles.chevronRightIcon}
-          alt=""
-          src="/chevronright-1.svg"
-        /></h2>
-
-
+        <h2 className={styles.seeAgain}>
+          랭킹 TOP10
+          <img
+            className={styles.chevronRightIcon}
+            alt=""
+            src="/chevronright-1.svg"
+          />
+        </h2>
       </div>
     </div>
-
-
   );
 };
 
