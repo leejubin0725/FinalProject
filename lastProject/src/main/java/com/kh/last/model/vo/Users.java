f0b01fca14cf74e02581d64d8af6414e114299f9
javis <jbl0744@naver.com>
2024-07-31 16:15:03 +0900

    구글로그인 33

--- lastProject/src/main/java/com/kh/last/model/vo/Users.java
+++ lastProject/src/main/java/com/kh/last/model/vo/Users.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 import lombok.Data;
 
 @Entity
@@ -11,9 +12,17 @@ import lombok.Data;
 public class Users {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq")
+    @SequenceGenerator(name = "user_seq", sequenceName = "USER_SEQ", allocationSize = 1)
     private Long id;
     private String name;
     private String password;
     private String phone;
+
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
+//    private Long id;
+//    private String name;
+//    private String password;
+//    private String phone;
 }
