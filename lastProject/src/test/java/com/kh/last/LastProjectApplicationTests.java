9df79573166285c5f1fb4263b96d2d9bf5085c5b
javis <jbl0744@naver.com>
2024-08-14 11:44:42 +0900

    테스트코드 사용 x 주석

--- lastProject/src/test/java/com/kh/last/LastProjectApplicationTests.java
+++ lastProject/src/test/java/com/kh/last/LastProjectApplicationTests.java
@@ -1,13 +1,13 @@
-package com.kh.last;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class LastProjectApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.kh.last;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class LastProjectApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
